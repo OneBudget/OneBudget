30a605e19a1ecb15c957b89c83e1a022f57a3278
Josh Johnson <josh.johnson@leafyacre.com>
2018-02-11 00:18:54 -0500

    Remove SaveBankAccount Command

--- src/Sdk/OneBudget.Sdk.Abstractions/Commands/SaveBankAccount/ISaveBankAccountCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace OneBudget.Sdk.Commands
-{
-    public interface ISaveBankAccountCommand
-    {
-        Task Execute(SaveBankAccountModel model);
-    }
-}
